1e2f6fd8014b4a01723c7741b65ad1fa3e688b85
ultra-pool <ultrapoolcom@gmail.com>
2021-04-30 14:44:02 -0700

    file fixes

--- node/src/command.rs
+++ node/src/command.rs
@@ -23,7 +23,7 @@ use node_template_runtime::Block;
 
 impl SubstrateCli for Cli {
 	fn impl_name() -> String {
-		"Substrate Node".into()
+		"Wehub Node".into()
 	}
 
 	fn impl_version() -> String {
